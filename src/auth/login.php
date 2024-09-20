89d075b61f8af7b5fb9558374fb5518fde6626e0
Andrey Sheshko <116847097+apachaihop@users.noreply.github.com>
2024-09-20 19:08:26 +0300

    Update login.php

--- src/auth/login.php
+++ src/auth/login.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 include '../connection.php';
+if (isset($_SESSION['user_id'])) {
+    header("Location: /lab1/index.php");
+    exit();
+}
 try{
 $conn = getConnection();
 
